26724987f51e20c8b09056b06e1e7f3882c16c54
itzme-challa <challawaheed4@gmail.com>
2025-10-26 08:47:29 +0530

    Update google-sheets.ts

--- src/utils/google-sheets.ts
+++ src/utils/google-sheets.ts
@@ -1,5 +1,5 @@
 export function getScriptUrl(sheetId: string): string {
   // In production, you'll need to deploy this as a Google Apps Script web app
   // and replace this URL with your actual deployment URL
-  return `https://script.google.com/macros/s/AKfycbzXIeO5djYOPXomJdtHg-ZM_MTb-v66KFkkMmV-h3IRPbYgqJ4QwaDDrR7spuoBUCZX/exec?action=`;
+  return `https://script.google.com/macros/s/AKfycbzGKnLezkfxGpHI1Gi3uwiq3EdnHva_V7k1LjL_CuHyQz8HWs7qJo5BbeUpmEZJlswm/exec?action=`;
 }
