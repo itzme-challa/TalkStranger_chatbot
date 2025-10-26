b78ce6e26ddc593daf6c77ab24d3ce9e97ba57d3
itzme-challa <challawaheed4@gmail.com>
2025-10-26 08:19:53 +0530

    Create google-sheets.ts

---
+++ src/utils/google-sheets.ts
@@ -0,0 +1,5 @@
+export function getScriptUrl(sheetId: string): string {
+  // In production, you'll need to deploy this as a Google Apps Script web app
+  // and replace this URL with your actual deployment URL
+  return `https://script.google.com/macros/s/AKfycbzWfgvP9SJ5dSmQH8ijIhNAodPyUynyCeWikRUzjKqJXfQiLgkv_RODOuMJJzY38hIW/exec?action=`;
+}
